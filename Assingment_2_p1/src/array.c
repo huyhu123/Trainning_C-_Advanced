0827c59508c17951f02c39b389442a2bda3544c7
huy <chuthienhuy2@gmail.com>
2023-08-21 08:37:55 +0700

    update

--- Assingment_2_p1/src/array.c
+++ Assingment_2_p1/src/array.c
@@ -6,7 +6,7 @@
 
 static int Clean_stdin()
 {
-    while(getchar() != '\n');
+    while (getchar() != '\n');
     return 1;
 }
 
@@ -16,8 +16,7 @@ static int Get_input_size()
     unsigned int i =0 ;
     char c;
 
-    while ((scanf("%d", &input) != 1 || input < 1 || input >50) && Clean_stdin())
-    {
+    while ((scanf("%d", &input) != 1 || input < 1 || input >50) && Clean_stdin()) {
         printf("\n*Warning:Failed! Please enter an interger between 0 and 50.\nEnter again:  ");
     }
     return input;
@@ -29,8 +28,7 @@ static int Get_input_int()
     unsigned int i =0 ;
     char c;
 
-    while ((scanf("%d", &input) != 1) && Clean_stdin())
-    {
+    while ((scanf("%d", &input) != 1) && Clean_stdin()) {
         printf("\n*Warning:Failed! Please enter an interger.\nEnter again:  ");
     }
     return input;
@@ -48,8 +46,7 @@ int *initialize_array(int size)
 
 void print_array(int *array, int size)
 {
-    for(int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         printf("%i ", array[i]);
     }
     printf("\n");
@@ -57,8 +54,7 @@ void print_array(int *array, int size)
 
 void get_array_data(int *array, int size)
 {
-    for(int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         printf("Enter element %d of the array: ", i);
         array[i] = Get_input_int();
     }
@@ -69,8 +65,7 @@ float calculate_average_value(int *array, int size)
 {
     float average = 0;
 
-    for(int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         average += array[i];
     }
 
@@ -81,10 +76,8 @@ int count_elements_less_than_average(int *array, int size, float average)
 {
     int count = 0;
 
-    for(int i = 0; i < size; i++)
-    {
-        if(array[i] < average)
-        {
+    for (int i = 0; i < size; i++) {
+        if (array[i] < average) {
             count++;
         }
     }
@@ -96,10 +89,8 @@ int count_elements_more_than_average(int *array, int size, float average)
 {
     int count = 0;
 
-    for(int i = 0; i < size; i++)
-    {
-        if(array[i] > average)
-        {
+    for (int i = 0; i < size; i++) {
+        if (array[i] > average) {
             count++;
         }
     }
@@ -112,19 +103,15 @@ void move_elements_array(int *array, int size)
     int left_index = 0;
     int right_index = size - 1;
 
-    while(left_index < right_index)
-    {
-        while(abs(array[left_index]) % 2 == 1 && left_index < right_index)
-        {
+    while (left_index < right_index) {
+        while (abs(array[left_index]) % 2 == 1 && left_index < right_index) {
             left_index++;            
         }
-        while(abs(array[right_index]) % 2 == 0 && left_index < right_index)
-        {
+        while (abs(array[right_index]) % 2 == 0 && left_index < right_index) {
             right_index--;
         }
 
-        if(left_index < right_index)
-        {
+        if (left_index < right_index) {
             SWAP(array[left_index], array[right_index]);
         } 
     }
