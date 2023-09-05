247a73dfc8dda8c0126d64e84ac8fab7257ffcc6
huy <chuthienhuy2@gmail.com>
2023-09-05 17:09:56 +0700

    add comment

--- Assingment_4_p1/inc/gemtek_lib.h
+++ Assingment_4_p1/inc/gemtek_lib.h
@@ -9,10 +9,27 @@
 #include <errno.h>
 #include <limits.h>
 
+/**
+ * @brief Calculates the square of an integer.
+ *
+ * @param x The integer value to be squared.
+ * @return The square of the input integer.
+ */
 double square(int x);
 
+/**
+ * @brief Calculates the cube of an integer.
+ *
+ * @param x The integer value to be cubed.
+ * @return The cube of the input integer.
+ */
 double cube(int x);
 
+/**
+ * @brief Reads an integer from the user and stores it in the provided memory location.
+ *
+ * @param value A pointer to the memory location where the input integer will be stored.
+ */
 void get_input_int(int *value);
 
 #endif
--- Assingment_4_p2/inc/gemtek_lib.h
+++ Assingment_4_p2/inc/gemtek_lib.h
@@ -4,11 +4,43 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/**
+ * @brief Calculates the square of an integer.
+ *
+ * This function takes an integer value and returns its square.
+ *
+ * @param x The integer value to be squared.
+ * @return The square of the input integer.
+ */
 double square(int x);
 
+/**
+ * @brief Calculates the cube of an integer.
+ *
+ * This function takes an integer value and returns its cube.
+ *
+ * @param x The integer value to be cubed.
+ * @return The cube of the input integer.
+ */
 double cube(int x);
 
+/**
+ * @brief Checks if a given number is a Fibonacci number.
+ *
+ * This function takes a floating-point number and checks if it is a Fibonacci number.
+ *
+ * @param n The number to be checked.
+ * @return 1 if the number is a Fibonacci number, 0 otherwise.
+ */
 int is_fibonacci(float n);
 
+/**
+ * @brief Reads an integer from the user and stores it in the provided memory location.
+ *
+ * This function prompts the user to enter an integer, which will be stored at the provided memory location.
+ *
+ * @param value A pointer to the memory location where the input integer will be stored.
+ */
 void get_input_int(int *value);
+
 #endif
