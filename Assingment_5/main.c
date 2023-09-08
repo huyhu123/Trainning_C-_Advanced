a3b818b611228e1af8199da79a4d47bc7f145187
huy <chuthienhuy2@gmail.com>
2023-09-08 16:56:37 +0700

    add space to swith case condition

--- Assingment_5/main.c
+++ Assingment_5/main.c
@@ -15,32 +15,32 @@ int main() {
         option = show_main_UI(out_dir, out_name);
         switch (option)
         {
-        case e_quit:
-            break;
-        case e_encode:
-            clrscr();
-            choose_encode_text_option(out_dir, out_name);
-            break;
-        case e_decode:
-            clrscr();
-            choose_decode_text_option(out_dir, out_name);
-            break;
-        case e_encode_file:
-            clrscr();
-            choose_encode_file_option(out_dir, out_name);
-            break;
-        case e_decode_file:
-            clrscr();
-            choose_decode_file_option(out_dir, out_name);
-            break;
-        case e_change_output_name:
-            clrscr();
-            change_output_name(out_name);
-            break;
-        case e_change_output_dir:
-            clrscr();
-            change_output_dir(out_dir);
-            break;
+            case e_quit:
+                break;
+            case e_encode:
+                clrscr();
+                choose_encode_text_option(out_dir, out_name);
+                break;
+            case e_decode:
+                clrscr();
+                choose_decode_text_option(out_dir, out_name);
+                break;
+            case e_encode_file:
+                clrscr();
+                choose_encode_file_option(out_dir, out_name);
+                break;
+            case e_decode_file:
+                clrscr();
+                choose_decode_file_option(out_dir, out_name);
+                break;
+            case e_change_output_name:
+                clrscr();
+                change_output_name(out_name);
+                break;
+            case e_change_output_dir:
+                clrscr();
+                change_output_dir(out_dir);
+                break;
         }
     }
 
